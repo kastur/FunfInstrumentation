8183b28a220155450afb33cd5a51d59b2048ce57
kastur <kastur@gmail.com>
2012-08-13 22:04:02 -0700

    Simple activity instruments lifecycle events.

--- InstrumentedApp/src/com/example/MyActivity.java
+++ InstrumentedApp/src/com/example/MyActivity.java
@@ -1,15 +1,43 @@
 package com.example;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.lang.Override;
+
 public class MyActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        doInstrument("onCreate");
         setContentView(R.layout.main);
+    }
+
+    @Override
+    protected void onDestroy() {
+        doInstrument("onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        doInstrument("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        doInstrument("onPause");
+        super.onPause();
+    }
+
+    private void doInstrument(String event) {
+        Intent notifyIntent = new Intent("edu.mit.media.funf.bgcollector.MainPipeline");
+        notifyIntent.putExtra("PKG_NAME", getPackageName());
+        notifyIntent.putExtra("CLS_NAME", getClass().getName());
+        notifyIntent.putExtra("EVENT_NAME", event);
+        startService(notifyIntent);
     }
 }
